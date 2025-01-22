031521ee342f5eeb9da5bd6e90dd17909a58df9a
Nikita Kharin <harin-1990@yandex.ru>
2025-01-22 18:34:29 +0400

    Добавьте файлы проекта.

---
+++ Mod17/Program.cs
@@ -0,0 +1,37 @@
+﻿// Общий интерфейс для всех аккаунтов
+public interface IAccount
+{
+    double Balance { get; set; }
+    double Interest { get; set; }
+    void CalculateInterest();
+}
+
+// Базовый класс 
+public abstract class AccountBase : IAccount
+{
+    public double Balance { get; set; }
+    public double Interest { get; set; }
+    public abstract void CalculateInterest();
+}
+
+// Конкретные реализации для каждого типа аккаунта
+public class RegularAccount : AccountBase
+{
+    public override void CalculateInterest()
+    {
+        Interest = Balance * 0.4;
+
+        if (Balance < 1000)
+            Interest -= Balance * 0.2;
+        else if (Balance >= 1000)
+            Interest -= Balance * 0.4;
+    }
+}
+
+public class SalaryAccount : AccountBase
+{
+    public override void CalculateInterest()
+    {
+        Interest = Balance * 0.5;
+    }
+}
